9e567a981f7e735e95abe1252e805bf464e0f7e3
Toan Nguyen <toannq177@gmail.com>
2019-04-17 10:59:09 -0400

    [f] some comments

--- python/square_sorted_arr.py
+++ python/square_sorted_arr.py
@@ -20,3 +20,15 @@ class Solution:
 # Test
 obj = Solution()
 print(obj.sortedSquares([-4,-1,0,3,10]))
+
+# First off, when the problem is short, write your own sort function
+# However, in interview, unless the problem is a sorting problem, like they ask you
+# to implement bubble sort/ quicksort, you can use the built-in sort function like above.
+# Second, python and other languages support a thing called compare function for sort.
+# Read more: https://docs.python.org/3.3/howto/sorting.html
+# For example:
+# >>> def numeric_compare(x, y):
+#         return x - y
+# >>> sorted([5, 2, 4, 1, 3], cmp=numeric_compare)
+# [1, 2, 3, 4, 5]
+# For this problem, we can just write a compare function that compare elements by their abs value, right?
