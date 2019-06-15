09dc1bb61d6fe656c7c094f341a8dfd1d60887c9
zee-nguyen <zinguyenn@gmail.com>
2019-06-15 13:24:08 -0400

    sol for https://leetcode.com/problems/sum-of-digits-in-the-minimum-number

---
+++ arrays/sum_digits_of_min.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/sum-of-digits-in-the-minimum-number/
+# Given an array A of positive integers, let S be the sum of the digits of the minimal element of A.
+# Return 0 if S is odd, otherwise return 1.
+
+from typing import List
+
+class Solution:
+    def sumOfDigits(self, A: List[int]) -> int:
+        smallest = float("inf")
+        sumDigits = 0
+        for i in A:
+            smallest = min(smallest, i)
+
+        while(smallest):
+            sumDigits += smallest % 10
+            smallest //= 10
+        return 0 if sumDigits % 2 != 0 else 1
+
+# A = [34,23,1,24,75,33,54,8]
+A = [99,77,33,66,55]
+test = Solution()
+print(test.sumOfDigits(A))
