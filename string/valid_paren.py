53dc3c8658b12b309a3ac57d037c15d55abf4abe
zee-nguyen <zinguyenn@gmail.com>
2019-08-21 18:02:01 -0400

    shorter sol for https://leetcode.com/problems/valid-parentheses/

--- string/valid_paren.py
+++ string/valid_paren.py
@@ -2,26 +2,41 @@
 
 class Solution:
     def isValid(self, s: str) -> bool:
+        # if not s:
+        #     return True
+        # stack = []
+        # for c in s:
+        #     if c == "(" or c == "{" or c == "[":
+        #         stack.append(c)
+        #     else:
+        #         # pop from empty stack
+        #         if not stack:
+        #             return False
+        #         # check for matching pair
+        #         if c == ")":
+        #             if stack.pop() != "(":
+        #                 return False
+        #         elif c == "}":
+        #             if stack.pop() != "{":
+        #                 return False
+        #         else:
+        #             if stack.pop() != "[":
+        #                 return False
+                        
+        # # True only if stack is empty
+        # return len(stack) == 0
+        
         if not s:
             return True
         stack = []
+        d = {
+            "(": ")",
+            "[": "]",
+            "{": "}"
+        }
         for c in s:
-            if c == "(" or c == "{" or c == "[":
+            if c in d.keys():
                 stack.append(c)
-            else:
-                # pop from empty stack
-                if not stack:
-                    return False
-                # check for matching pair
-                if c == ")":
-                    if stack.pop() != "(":
-                        return False
-                elif c == "}":
-                    if stack.pop() != "{":
-                        return False
-                else:
-                    if stack.pop() != "[":
-                        return False
-                        
-        # True only if stack is empty
-        return len(stack) == 0
+            if not stack or c != d[stack.pop()]:
+                return False
+        return stack == []
