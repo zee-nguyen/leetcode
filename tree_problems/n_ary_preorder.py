192c2f09104d81ecc9bf38ef91f6244947d7d8ae
Hieu Luu <zinguyenn@gmail.com>
2019-09-09 09:43:25 -0400

    updated solution with comments

--- tree_problems/n_ary_preorder.py
+++ tree_problems/n_ary_preorder.py
@@ -11,28 +11,36 @@ class Node:
 class Solution:
     def preorder(self, root: 'Node') -> List[int]:
         # Recursively
-        def helper(root, res):
-            if not root:
-                return
-            res.append(root.val)
-            for c in root.children:
-                helper(c, res)
-        res = []
-        helper(root, res)
-        return res
+        # def helper(root, res):
+        #     if not root:
+        #         return
+        #     res.append(root.val)
+        #     for c in root.children:
+        #         helper(c, res)
+        # res = []
+        # helper(root, res)
+        # return res
     
         # Iteratively
         if not root:
             return [] 
         
-        stack = []
-        res = []
-        stack.append(root)
+        # stack = []
+        # res = []
+        # stack.append(root)
+        # 3 lines above can be this
+        stack, res = [root], []
+        
         while stack:
             top = stack.pop()
-            i = len(top.children)-1
+            # i = len(top.children)-1
             res.append(top.val)
-            while i >= 0 and top.children:
-                stack.append(top.children[i])
-                i -= 1
+            # while i >= 0 and top.children:
+                # stack.append(top.children[i])
+                # i -= 1
+                
+            # use list.extend() to iterate over a list and append each item
+            # use top.children[::-1] to add the children in reversed order
+            stack.extend(top.children[::-1])
+            
         return res
