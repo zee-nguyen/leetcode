12f9c6fbcdbd74be4fe2abce8096d245de4b9155
Zee Nguyen <zinguyenn@gmail.com>
2019-10-04 14:54:20 -0400

    sol for https://leetcode.com/problems/symmetric-tree/

---
+++ tree/symmetric_tree_iterative.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/symmetric-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        
