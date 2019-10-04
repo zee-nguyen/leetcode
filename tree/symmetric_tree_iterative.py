095d360d24f6e115530726513a0b9eeb7cad4996
Zee Nguyen <zinguyenn@gmail.com>
2019-10-04 14:57:15 -0400

    sol for https://leetcode.com/problems/symmetric-tree/ refactor

--- tree/symmetric_tree_iterative.py
+++ tree/symmetric_tree_iterative.py
@@ -9,4 +9,26 @@
 
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
-        
+        # Iterative with stack
+        if not root or (not root.left and not root.right):
+            return True
+        else:
+            s = []
+            s.append(root.left)
+            s.append(root.right)
+            while s:
+                top = s.pop()
+                second = s.pop()
+
+                if not top and not second:
+                    continue
+                if not top or not second or top.val != second.val:
+                    return False
+                
+                s.append(top.left)
+                s.append(second.right)
+
+                s.append(top.right)
+                s.append(second.left)
+                    
+        return True
