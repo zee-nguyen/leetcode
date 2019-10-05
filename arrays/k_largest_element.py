bbdeb690bf234f815993a01cbe206cb87adf9307
Zee Nguyen <zinguyenn@gmail.com>
2019-10-05 11:49:45 -0400

    sol for https://leetcode.com/problems/kth-largest-element-in-an-array/ slightly different sol

--- arrays/k_largest_element.py
+++ arrays/k_largest_element.py
@@ -7,16 +7,26 @@ class Solution:
         # use min heap
         # all the code below can be written as: return heapq.nlargest(k, nums)[-1]
         
+        # heap = []
+        
+        # for num in nums:
+        #     if len(heap) == k:
+        #         smallest = heapq.heappop(heap)
+        #         if num < smallest:
+        #             heapq.heappush(heap, smallest)
+        #         else:
+        #             heapq.heappush(heap, num)
+        #     else:
+        #         heapq.heappush(heap, num)
+        
+        # return heapq.heappop(heap)
+
         heap = []
         
         for num in nums:
-            if len(heap) == k:
-                smallest = heapq.heappop(heap)
-                if num < smallest:
-                    heapq.heappush(heap, smallest)
-                else:
-                    heapq.heappush(heap, num)
-            else:
-                heapq.heappush(heap, num)
+            heapq.heappush(heap, num)
+            
+            if len(heap) == k+1:
+                heapq.heappop(heap)
         
         return heapq.heappop(heap)
