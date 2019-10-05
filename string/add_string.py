4f8796c75627a917fc14fb2431c679f4df77c2e3
Zee Nguyen <zinguyenn@gmail.com>
2019-10-05 09:05:29 -0400

    not converting to int

--- string/add_string.py
+++ string/add_string.py
@@ -1,17 +1,19 @@
 # https://leetcode.com/problems/add-strings/
 
-class Solution:
+class AddString:
     def addStrings(self, num1: str, num2: str) -> str:
-        def str_to_digit(num):
-            ''' turn a string into digit '''
-            arr = list(num)
-            power = 0
-            ret = 0
-            for i in reversed(range(len(arr))):
-                ret += 10**power * (ord(arr[i]) - ord("0"))
-                power += 1
-            
-            return ret
+        num1 = list(num1)
+        num2 = list(num2)
         
-        total = str_to_digit(num1) + str_to_digit(num2)
-        return str(total)
+        res, carry = "", 0
+        
+        while num1 or num2:
+            n1, n2 = 0, 0
+            n1 = ord(num1.pop()) - ord("0") if num1 else 0
+            n2 = ord(num2.pop()) - ord("0") if num2 else 0
+            total = n1 + n2 + carry
+            carry = total // 10
+            res = str(total % 10) + res
+        if carry:
+            res = str(carry) + res
+        return res
