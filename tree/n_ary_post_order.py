b8848204c29bbce4fa1df5d19e4589ea211c9213
Zee Nguyen <zinguyenn@gmail.com>
2019-10-07 08:19:07 -0400

    sol with 2 stacks and sol recursive https://leetcode.com/problems/n-ary-tree-postorder-traversal/

--- tree/n_ary_post_order.py
+++ tree/n_ary_post_order.py
@@ -11,15 +11,31 @@ class Solution:
     def postorder(self, root: 'Node') -> List[int]:
         # Iterative
         # The idea is to have root - right - left using a stack, then reverse the list
+        # if not root:
+        #     return []
+        
+        # stack, ret = [root], []
+        
+        # while stack:
+        #     top = stack.pop()
+        #     if top:
+        #         ret.append(top.val)
+        #     stack.extend(top.children)
+        
+        # return ret[::-1]
+
         if not root:
             return []
         
-        stack, ret = [root], []
+        ret, s1, s2 = [], [root], []
         
-        while stack:
-            top = stack.pop()
-            if top:
-                ret.append(top.val)
-            stack.extend(top.children)
+        while s1:
+            top = s1.pop()
+            s2.append(top)
+            for c in top.children:
+                s1.append(c)
         
-        return ret[::-1]
+        while s2:
+            ret.append(s2.pop().val)
+            
+        return ret
