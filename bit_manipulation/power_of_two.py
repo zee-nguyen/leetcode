714d8be2b5a9b18beb3e33e8531fd36aef7a685b
Zee Nguyen <zinguyenn@gmail.com>
2020-08-17 10:30:36 -0400

    sol for https://leetcode.com/problems/power-of-two/

---
+++ bit_manipulation/power_of_two.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/power-of-two/
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 0:
+            return False
+        return True if not (n & n-1) else False
