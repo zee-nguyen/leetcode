c348070a9d1ddd9d1c021f713fc6bf93517e1af0
Zee Nguyen <zinguyenn@gmail.com>
2020-08-28 16:16:47 -0400

    clean up

---
+++ dutch_flag.py
@@ -0,0 +1,4 @@
+'''
+Write a program that takes an array A and an index i into A, and rearranges the elements such that all elements less thatn A[i] appear first, followed by elements equal to the pivot followed by elements grater than the pivots.
+'''
+
