e0ca0609cd48aa84c97c639acf946caf8d13c8cc
Zee Nguyen <zinguyenn@gmail.com>
2020-11-02 08:51:42 -0500

    updated per comment

--- intersection_of_3_sorted_arrays.py
+++ intersection_of_3_sorted_arrays.py
@@ -2,16 +2,16 @@
 
 class Solution:
     def arraysIntersection(self, arr1: List[int], arr2: List[int], arr3: List[int]) -> List[int]:
-        c = collections.Counter(arr1)
-        track = dict()
-        res = []
+        counter = collections.Counter(arr1)
+        
         for el in arr2:
-            if el in c:
-                track[el] = 1
+            if el in counter:
+                counter[el] += 1
         for el in arr3:
-            if el in track:
-                res.append(el)
-        return res
+            if el in counter:
+                counter[el] += 1
+        
+        return [k for k in counter.keys() if counter[k] >= 3]
 
         # Sol 2:
         # return [k for k, v in collections.Counter(arr1+arr2+arr3).items() if v == 3]
